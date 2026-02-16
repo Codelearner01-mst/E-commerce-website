b525352b89e7c32c8199a81f037fa21664a0efa3
Paul Code <kwabenap603@gmail.com>
2026-02-16 14:42:53 +0000

    feat: Add update cart success message

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -9,12 +9,18 @@ import { addToCartAndShowMessage } from "../utils/cart-controller.js";
 
 const cartCount = document.getElementById("cart-count");
 const hamburgerButton = document.getElementById("hamburger-btn");
+const cartButton = document.getElementById("cart-btn");
 const dropDownMenu = document.getElementById("drop-menu");
 const products = document.querySelector(".product-list");
+const updateCartSuccessMessage = document.querySelector(".update-cart-success");
+
+cartButton.addEventListener("click", () => {
+  window.location.href = "carts.html";
+});
 
 const carts = loadCarts();
 
-addToCartAndShowMessage(products, carts, cartCount);
+addToCartAndShowMessage(products, carts, cartCount, updateCartSuccessMessage);
 
 const count = cartsCounter(carts);
 displayCartsCount(cartCount, count);
