f35240ac7621a8ebdddb3ae2a6ed7ae6deb8d041
Paul Code <kwabenap603@gmail.com>
2026-03-02 08:52:10 +0000

    feat: Change to quantity control button when product is added to cart and display success  message

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,5 +1,19 @@
 import { renderProduct } from "../components/render.js";
+import { addToCartOrControlQuantity } from "../utils/cart-controller.js";
+import { loadCarts } from "../utils/saveUtils.js";
+
+const carts = loadCarts();
 
 const productContainer = document.getElementById("product-container");
-
 renderProduct(productContainer);
+
+const addTocartBtn = document.getElementById("add-cart-btn");
+const addToCartSuccessMessage = document.querySelector(".added-cart-success");
+const cartsCount = document.getElementById("cart-count");
+
+addTocartBtn.addEventListener("click", () => {
+  const card = addTocartBtn.closest(".product-card");
+  addToCartOrControlQuantity(card, carts, addToCartSuccessMessage, cartsCount);
+  card.querySelector(".quantity-control").style.backgroundColor = "white";
+  card.querySelector(".quantity-control").style.border = "none";
+});
