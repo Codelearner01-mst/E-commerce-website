3f30c91be193c8a0e1406483ac5896c904331ed6
Paul Code <kwabenap603@gmail.com>
2026-03-02 10:37:48 +0000

    Feat: Display quantity control UI for product alreadyin cart, when page reloads
    Display carts count when page reloads

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,6 +1,8 @@
 import { renderProduct } from "../components/render.js";
 import { addToCartOrControlQuantity } from "../utils/cart-controller.js";
 import { loadCarts } from "../utils/saveUtils.js";
+import { setProductQuantityControl } from "../utils/shared.js";
+import { displayCartsCount } from "../utils/shared.js";
 
 const carts = loadCarts();
 
@@ -10,6 +12,23 @@ renderProduct(productContainer);
 const addTocartBtn = document.getElementById("add-cart-btn");
 const addToCartSuccessMessage = document.querySelector(".added-cart-success");
 const cartsCount = document.getElementById("cart-count");
+const currentProduct = JSON.parse(sessionStorage.getItem("currentProduct"));
+console.log(currentProduct);
+
+if (carts.some((cart) => cart.id === currentProduct.id)) {
+  console.log("This logic runs");
+  const index = carts.findIndex((c) => c.id === currentProduct.id);
+  const card = productContainer.querySelector(".product-card");
+  setProductQuantityControl(
+    card.querySelector(".quantity-control"),
+    carts[index],
+    carts,
+    addToCartSuccessMessage,
+    cartsCount,
+  );
+  card.querySelector(".quantity-control").style.backgroundColor = "white";
+  card.querySelector(".quantity-control").style.border = "none";
+}
 
 addTocartBtn.addEventListener("click", () => {
   const card = addTocartBtn.closest(".product-card");
@@ -17,3 +36,5 @@ addTocartBtn.addEventListener("click", () => {
   card.querySelector(".quantity-control").style.backgroundColor = "white";
   card.querySelector(".quantity-control").style.border = "none";
 });
+
+displayCartsCount(cartsCount, carts);
