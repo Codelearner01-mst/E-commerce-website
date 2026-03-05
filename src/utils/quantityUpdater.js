840f1eb700fa579029121c4e3192890e57720310
Paul Code <kwabenap603@gmail.com>
2026-03-05 20:30:39 +0000

    Remove function duplication
    Add comment to code

--- src/utils/quantityUpdater.js
+++ src/utils/quantityUpdater.js
@@ -1,3 +1,8 @@
+/**
+ * Decrease cart quantity by 1
+ * @param {HTMLElement} ele - quantity display element
+ * @param {{id:number,name:string,price:number,image:string,quantity:number}} cart
+ */
 export function decreaseQuantity(cart, ele) {
   if (cart.quantity > 1) {
     cart.quantity -= 1;
@@ -5,6 +10,11 @@ export function decreaseQuantity(cart, ele) {
   }
 }
 
+/**
+ * Increase cart quantity by 1
+ * @param {HTMLElement} ele - quantity display element
+ * @param {{id:number,name:string,price:number,image:string,quantity:number}} cart
+ */
 export function increaseQuantity(cart, ele) {
   cart.quantity += 1;
   ele.textContent = cart.quantity;
--- src/utils/shared.js
+++ src/utils/shared.js
@@ -30,28 +30,6 @@ export function toggleDropdownMenu(dropdown) {
   dropdown.classList.toggle("hidden");
 }
 
-/**
- * Decrease cart quantity by 1
- * @param {HTMLElement} ele - quantity display element
- * @param {{id:number,name:string,price:number,image:string,quantity:number}} cart
- */
-export function decreaseQuantity(cart, ele) {
-  if (cart.quantity > 1) {
-    cart.quantity -= 1;
-    ele.textContent = cart.quantity;
-  }
-}
-
-/**
- * Increase cart quantity by 1
- * @param {HTMLElement} ele - quantity display element
- * @param {{id:number,name:string,price:number,image:string,quantity:number}} cart
- */
-export function increaseQuantity(cart, ele) {
-  cart.quantity += 1;
-  ele.textContent = cart.quantity;
-}
-
 //Set the quantity UI and control for product
 export function setProductQuantityControl(ele, cart, carts, msgEle, countEle) {
   if (!Array.isArray(carts)) {
