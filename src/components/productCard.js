a52aacd2168c5231bbbc8cfbbf40f5735e42f85d
Paul Code <kwabenap603@gmail.com>
2026-03-27 11:53:18 +0000

    Restore productCard.js. Add lazy loading to images

---
+++ src/components/productCard.js
@@ -0,0 +1,23 @@
+export function productCardHTML(product) {
+  return `  <div class="product-card" id="produtct-${product.id}">
+              <div class="product-image-container">
+                <img
+                  src="${product.image}"
+                  alt="${product.name}"
+                  class="product-image"
+                  loading="lazy"
+                />
+              </div>
+              <div class="product-info">
+                <h3 class="product-title">${product.name}</h3>
+                <div class="flex justify-between items-center">
+                  <p class="product-price">$${product.price}</p>
+                  <div class="cart-actions-container">
+                    <button class="add-cart-btn btn-primary">
+                      +Add To Cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+}
